cce1076f04a842cbe44e8aaaa6823ac0c7cd1811
Borja <alu0100498820@ull.edu.es>
2015-11-20 23:06:40 +0000

    clase pregunta y respuesta

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -1,61 +1,47 @@
 var AbstractQuiz = require('../models/abstract_quiz_model.js');
+var Respuesta = require('../models/respuesta.js');
+var PreguntaLarga = require('../models/pregunta_larga.js');
+var PreguntaCorta = require('../models/pregunta_corta.js');
 
 function Quiz() {
   AbstractQuiz.call(this);
   this.q.push(
-    { pregunta: '¿Capital de Italia?',
-      respuesta: function(x) {
-        return (/^\s*roma\s*$/i).exec(x);
-      }
+    { pregunta: new PreguntaCorta('¿Capital de Italia?'),
+      respuesta: new Respuesta(/^\s*roma\s*$/i)
     },
-    {
-      pregunta: '¿Quien reinaba en España cuando se descubrió América?',
-      respuesta: function(x) {
-        if ((/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)\b/i).exec(x)) {
-          return true;
-        }
-        if ((/\breyes\s+cat[oó]licos\b/i).exec(x)) { return true; }
-        return false;
-      },
+
+    { pregunta: new PreguntaLarga('¿Quien reinaba en España cuando se descubrió América?'),
+      respuesta: new Respuesta(/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)|(reyes\s+cat[oó]licos)\b/i)
     },
     //*************************************************************************************************************************************
-     { pregunta: '¿Quien es el CEO de Apple Inc.?',
-      respuesta: function(x) {
-        return (/\s*tim cook\s*$/i).exec(x);
-      }
+    { pregunta: new PreguntaCorta('¿Quien es el CEO de Apple Inc.?'),
+      respuesta: new Respuesta(/^\s*tim cook\s*$/i)
     },
 
-    { pregunta: 'Lenovo, la empresa que fabrica de ordenadores, tablets y smartphones, es el acronimo de . . .',
-      respuesta: function(x) {
-        return (/\s*nueva leyenda\s*$/i).exec(x);
-      }
-    },
-    
-     { pregunta: '¿Cuando se creo la ULL (Universidad de la Laguna)?',
-      respuesta: function(x) {
-        return (/\s*1927\s*$/i).exec(x);
-      }
+    { pregunta: new PreguntaCorta('Lenovo, la empresa que fabrica de ordenadores, tablets y smartphones, es el acronimo de . . .'),
+      respuesta: new Respuesta(/^\s*nueva leyenda\s*$/i)
     },
 
-     { pregunta: '¿Cual es el equipo de la NBA con mas campeonatos?',
-      respuesta: function(x) {
-        return (/\s*boston celtics\s*$/i).exec(x);
-      }
+    { pregunta: new PreguntaCorta('¿Cuando se creo la ULL (Universidad de la Laguna)?'),
+      respuesta: new Respuesta(/^\s*1927\s*$/i)
     },
-        
-     { pregunta: '¿Quien reinaba en España cuando el Imperio español alcanzo su cúspide territorial?',
-      respuesta: function(x) {
-        return (/\s*carlos IV\s*$/i).exec(x);
-      }
+ 
+    { pregunta: new PreguntaCorta('¿Cual es el equipo de la NBA con mas campeonatos?'),
+      respuesta: new Respuesta(/^\s*boston celtics\s*$/i)
     },
+
+    { pregunta: new PreguntaCorta('¿Quien reinaba en España cuando el Imperio español alcanzo su cúspide territorial?'),
+      respuesta: new Respuesta(/^\s*carlos IV\s*$/i)
+    },
+
     //*************************************************************************************************************************************    
     
     { /* Código inseguro. ¡No ejecute esta pregunta salvo en un
          entorno en el que el código del "alumno" sea fiable!
        */
-  pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+ 
-  'que recibe un número y devuelve el cuadrado',
-      respuesta: function(x) {
+      pregunta: new PreguntaLarga('Escriba una función JavaScript de nombre <tt>square</tt> '+
+      'que recibe un número y devuelve el cuadrado'),
+      respuesta: new Respuesta( function(x) {
         try {
           eval(x); /* DANGER DANGER DANGER */
           var z = Math.floor(Math.random()*100);
@@ -65,7 +51,7 @@ function Quiz() {
           return false;
         }
         return false;
-      }
+      })
     }
   );
 
@@ -77,24 +63,28 @@ function Quiz() {
       var n1 = Math.randomInt(9)+1;
       var n2 = Math.randomInt(9)+1;
       self.q.push(
-        { pregunta: '¿ '+n1+'x'+n2+" ?",
-          respuesta: function(x) {
+        { pregunta: new PreguntaCorta('¿ '+n1+'x'+n2+"= ?"),
+          respuesta: new Respuesta(function(x) {
             return (x == n1*n2);
-        }
-      })
+        })
+      });
     })();
   }
 }
 
 Quiz.prototype = new AbstractQuiz();
 Quiz.prototype.constructor = Quiz;
-
 Quiz.prototype.numQuestions = function() {
   return this.q.length;
 }
-
 Quiz.prototype.getQ = function(x){
-  return this.q[x]['pregunta'];
+  return this.q[x]['pregunta'].get_pregunta();
+}
+Quiz.prototype.getQuestion = function(x){
+  return x['pregunta'].get_pregunta();
+}
+Quiz.prototype.getArea = function(x){
+  return x['pregunta'].get_area();
 }
 
 module.exports = Quiz;
